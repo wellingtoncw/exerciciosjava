0c7d55544a5d7348b680d207de0646629924e154
wellingtoncw <wellingtoncw7@gmail.com>
2021-02-27 11:01:48 -0300

    Cria MegaSena
    gera 6 numero aleatorios utilizando Random

--- MegaSena/src/MegaSena.java
+++ MegaSena/src/MegaSena.java
@@ -1,5 +1,24 @@
+import java.util.Random;
+
 public class MegaSena {
     public static void main(String[] args) {
-        
+        Random generate = new Random(); // instancia um objeto do tipo Random e guarda na variavel generate
+
+        System.out.println("===========LACO DE REPETICAO COM FOR===========");
+
+        for (int i=0; i<6; i++) { //laço for que vai de 0 a 5 (6 repetições)
+
+            int number = generate.nextInt(60); //executa o metodo nextInt e guarda na variavel number
+            System.out.println("Numero: " +number);
+        }
+
+        System.out.println("===========LACO DE REPETICAO COM WHILE===========");
+
+        int i = 0;
+        while (i < 6){ //enquanto o i menor que 6
+            int number = generate.nextInt(60); //executa o metodo nextInt e guarda na variavel number
+            System.out.println("Numero: " +number); //mostra o numero
+            i++; //adiciona 1 na variavel i (contador) -> necessário para sair do loop
+        }
     }
 }
